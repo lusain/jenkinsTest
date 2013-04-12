c7a9dba3af6bfebf89d019a97bca87acfbc1f84b
Lukasz Pikor <lukasz.pikor@synaway.com>
2013-04-12 13:07:08 +0200

    Third commint (navigation controller)

--- jenkinsTestApp/jenkinsTestAppViewController.m
+++ jenkinsTestApp/jenkinsTestAppViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad
 {
+    [self.button setBackgroundColor:[UIColor redColor]];
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
